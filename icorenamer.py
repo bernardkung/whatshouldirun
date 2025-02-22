03dd27d73ce93dacf1e8af8ab0d99419f4b6dd7d
bernard kung <speak.freely@gmail.com>
2025-02-22 02:20:43 -0500

    housekeeping

--- icorenamer.py
+++
@@ -1,8 +0,0 @@
-import os
-
-filenames = os.listdir('./public/images/class_icons/')
-
-for filename in filenames:
-  old_file = os.path.join(f'./public/images/class_icons/{filename}')
-  new_file = os.path.join(f'./public/images/class_icons/{filename.replace("_", "-")}')
-  os.rename(old_file, new_file)
